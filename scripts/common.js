36e0b0e7199ad8200ece0a3ab49c7aabc14e8729
vicchen666 <vic2000.chen@gmail.com>
2022-03-09 20:35:00 +0800

    Test 0.16

--- scripts/common.js
+++ scripts/common.js
@@ -1,6 +1,8 @@
+{
 $('.startoptions div:nth-child(1)').click(function() { 
-    $(this).parent().hind();
+    $(this).parent().hide();
 });
 $('.startoptions div:nth-child(3)').click(function() {
     window.close();
 });
+}
