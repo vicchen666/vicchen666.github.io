3b02ea93ed621850bb7c93475e91a0952e57e1bb
vicchen666 <vic2000.chen@gmail.com>
2022-03-11 22:09:59 +0800

    Test 0.17

--- scripts/common.js
+++ scripts/common.js
@@ -1,8 +1,11 @@
-{
+let x = 0;
 $('.startoptions div:nth-child(1)').click(function() { 
     $(this).parent().hide();
 });
 $('.startoptions div:nth-child(3)').click(function() {
     window.close();
 });
-}
+$('button').click(function() {
+    x += 1;
+    $('p').text(x);
+});
