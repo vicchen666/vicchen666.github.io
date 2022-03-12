f71fae505b9670ed8d3ff38b1ad6987a6b03e1b5
vicchen666 <vic2000.chen@gmail.com>
2022-03-12 12:34:11 +0800

    Test 0.23

--- scripts/common.js
+++ scripts/common.js
@@ -2,13 +2,18 @@ let x = 0;
 let data = $.ajax({
     type: "GET",
     url: "/data.json",
-    dataType: "json",
+    dataType: "json"
 }).done(() => {
     $('body').prepend('<button>this</button>');
     data = data.responseJSON;
     $('button').click(function() {
         x += 1;
-        $('p').text(data.test[0]);
+        $('p').text(data.test[x]);
+        $.ajax({
+            type: "POST",
+            url: "/data.json",
+            data: {test:[x]}
+        });
     });
 });
 $('.startoptions div:nth-child(1)').click(function() { 
