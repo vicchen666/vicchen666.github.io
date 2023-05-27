6f55227b373f4e0de218ba288dbe20138799672c
vic chen <vic2000.chen@gmail.com>
2023-05-27 16:01:21 +0800

    Crafting Table 0.31

--- scripts/common.js
+++ scripts/common.js
@@ -28,8 +28,8 @@ $("#SearchInput").on("input", function () {
 });
 
 $("#SearchResults").on("click", ".editRecipe", function() {
+    $("#Editor .page").remove();
     let page = "", editrecipe = names.indexOf($(this).parent().children("a:first").html().replace("'", "\\\\'"));
-    console.log(editrecipe)
     for (let i = 0; i < slots[editrecipe].length; i++) {
         page = "<div class=\"page\"><div class=\"ui-Crafting_Table\"><div class=\"ui-input\"><div class=\"ui-row\"><span class=\"invslot\"><span class=\"invslot-item\"></span></span><span class=\"invslot\"><span class=\"invslot-item\"></span></span><span class=\"invslot\"><span class=\"invslot-item\"></span></span></div><div class=\"ui-row\"><span class=\"invslot\"><span class=\"invslot-item\"></span></span><span class=\"invslot\"><span class=\"invslot-item\"></span></span><span class=\"invslot\"><span class=\"invslot-item\"></span></span></div><div class=\"ui-row\"><span class=\"invslot\"><span class=\"invslot-item\"></span></span><span class=\"invslot\"><span class=\"invslot-item\"></span></span><span class=\"invslot\"><span class=\"invslot-item\"></span></span></div></div><span class=\"ui-arrow\"></span><span class=\"ui-output\"><span class=\"invslot invslot-large\"><span class=\"invslot-item\"></span></span></span></div><table class=\"Attributes\"><tr><th></th><th>Slots</th><th>Items</th><th>Amounts</th><th><button class=\"addAttribute\">ADD</button></th></tr>";
         for (let j = 0; j < slots[editrecipe][i].length; j++) {
@@ -37,13 +37,22 @@ $("#SearchResults").on("click", ".editRecipe", function() {
         }
         page = page + "<tr><td>" + (slots[editrecipe][i].length + 1) + ".</td><td><input value=\"0\"></td><td><input value=\"" + items[editrecipe][i][slots[editrecipe][i].length].replace("\\", "") + "\"></td><td><input value=\"" + amounts[editrecipe][i][slots[editrecipe][i].length] + "\"></td><td><button class=\"removeAttribute\">✖</button></td></tr>";
         page = page + "</table></div>"
-        $("#Editor br:nth(1)").before(page);
+        $("#Editor br:eq(1)").before(page);
+    }
+    $("#Search").hide();
+    $("#Pages").html("1 / " + $("#Editor .page").length);
+    $("#RecipeID").attr("value", names[editrecipe]);
+    $("#Editor").show();
+    for (let j = 0; j < $("#Editor .page").length; j++) {
+        $("#Editor .page").hide();
+        $("#Editor .page:eq(" + j + ")").show();
+        update_crafting_table();
     }
     $("#Editor .page").hide();
     $("#Editor .page:first").show();
-    $("#Search").hide();
-    $("#Pages").html("1 / " + $("#Editor .page").length);
-    $("#Editor").show();
+});
+$("#Editor").on("input", "input:not([id])", () => {
+    update_crafting_table();
 });
 
 $("#Editor").on("click", "#Cancel, #Save", () => {
@@ -54,7 +63,7 @@ $("#Editor").on("click", "#Cancel, #Save", () => {
 $("#Editor").on("click", ".addAttribute", function() {  // add an attribute: slots, item, amount
     $(this).parents("tbody").children(":first").after("<tr><td>1.</td><td><input></td><td><input></td><td><input value=\"1\"></td><td><button class=\"removeAttribute\">✖</button></td></tr>");
     for (let i = 1; i < $(this).parents("tbody").children("tr").length; i++) {
-        $(this).parents("tbody").children(":nth(" + i + ")").children(":first").html(i + ".");
+        $(this).parents("tbody").children(":eq(" + i + ")").children(":first").html(i + ".");
     }
 });
 
@@ -63,15 +72,17 @@ $("#Editor").on("click", ".removeAttribute", function() {  // remove an attribut
         let tbody = $(this).parents("tbody");
         $(this).parents("tr").remove();
         for (let i = 1; i < tbody.children("tr").length; i++) {
-            tbody.children(":nth(" + i + ")").children(":first").html(i + ".");
+            tbody.children(":eq(" + i + ")").children(":first").html(i + ".");
         }
+        update_crafting_table();
     }
 });
 
 $("#Editor").on("click", "#AddPage", () => {
     $("#Editor .page:not(:hidden)").after("<div class=\"page\"><div class=\"ui-Crafting_Table\"><div class=\"ui-input\"><div class=\"ui-row\"><span class=\"invslot\"><span class=\"invslot-item\"></span></span><span class=\"invslot\"><span class=\"invslot-item\"></span></span><span class=\"invslot\"><span class=\"invslot-item\"></span></span></div><div class=\"ui-row\"><span class=\"invslot\"><span class=\"invslot-item\"></span></span><span class=\"invslot\"><span class=\"invslot-item\"></span></span><span class=\"invslot\"><span class=\"invslot-item\"></span></span></div><div class=\"ui-row\"><span class=\"invslot\"><span class=\"invslot-item\"></span></span><span class=\"invslot\"><span class=\"invslot-item\"></span></span><span class=\"invslot\"><span class=\"invslot-item\"></span></span></div></div><span class=\"ui-arrow\"></span><span class=\"ui-output\"><span class=\"invslot invslot-large\"><span class=\"invslot-item\"></span></span></span></div><table class=\"Attributes\"><tr><th></th><th>Slots</th><th>Items</th><th>Amounts</th><th><button class=\"addAttribute\">ADD</button></th></tr><tr><td>1.</td><td><input></td><td><input></td><td><input value=\"1\"></td><td><button class=\"removeAttribute\">✖</button></td></tr></table></div>");
-    $("#Editor .page:nth(" + ($("#Editor .page").index($(".page:not(:hidden)"))) + ")").hide();
+    $("#Editor .page:eq(" + ($("#Editor .page").index($(".page:not(:hidden)"))) + ")").hide();
     $("#Pages").html(($("#Editor .page").index($(".page:not(:hidden)")) + 1) + " / " + $("#Editor .page").length);
+    update_crafting_table();
 });
 
 $("#Editor").on("click", "#RemovePage", () => {
@@ -82,8 +93,8 @@ $("#Editor").on("click", "#RemovePage", () => {
             $("#Editor .page:first").show();
             $("#Pages").html("1 / " + $("#Editor .page").length);
         } else {
-            $("#Editor .page:nth(" + (page) + ")").remove();
-            $("#Editor .page:nth(" + (page - 1) + ")").show();
+            $("#Editor .page:eq(" + (page) + ")").remove();
+            $("#Editor .page:eq(" + (page - 1) + ")").show();
             $("#Pages").html(($("#Editor .page").index($(".page:not(:hidden)")) + 1) + " / " + $("#Editor .page").length);
         }
     }
@@ -96,8 +107,8 @@ $("#Editor").on("click", ".left-arrow", function() {
         $("#Editor .page:last").show();
         $("#Pages").html($("#Editor .page").length + " / " + $("#Editor .page").length);
     } else {
-        $("#Editor .page:nth(" + (page) + ")").hide();
-        $("#Editor .page:nth(" + (page - 1) + ")").show();
+        $("#Editor .page:eq(" + (page) + ")").hide();
+        $("#Editor .page:eq(" + (page - 1) + ")").show();
         $("#Pages").html(page + " / " + $("#Editor .page").length);
     }
     
@@ -110,8 +121,42 @@ $("#Editor").on("click", ".right-arrow", function() {
         $("#Editor .page:first").show();
         $("#Pages").html("1 / " + $("#Editor .page").length);
     } else {
-        $("#Editor .page:nth(" + (page) + ")").hide();
-        $("#Editor .page:nth(" + (page + 1) + ")").show();
+        $("#Editor .page:eq(" + (page) + ")").hide();
+        $("#Editor .page:eq(" + (page + 1) + ")").show();
         $("#Pages").html((page + 2) + " / " + $("#Editor .page").length);
     }
 });
+
+function update_crafting_table() {
+    if([...new Set($("#Editor .page:not(:hidden) tr td:nth-child(2) input").map(function() {
+        return $(this).val();
+    }).get().join("").split(""))].join("") === $("#Editor .page:not(:hidden) tr td:nth-child(2) input").map(function() {
+        return $(this).val();
+    }).get().join("")) {
+        for (let i = 0; i < 9; i++) {
+            $("#Editor .page:not(:hidden) .ui-row:eq(" + ~~(i/3) +") .invslot:eq(" + (i % 3) + ") .invslot-item *").remove();
+            $("#Editor .page:not(:hidden) .invslot-large .invslot-item *").remove();
+            $("#Editor .page:not(:hidden) .ui-row:eq(" + ~~(i/3) +") .invslot:eq(" + (i % 3) + ") .invslot-item").append("<div style=\"font-size: 12px;\">" + (i + 1) + "</div>");
+            $("#Editor .page:not(:hidden) .invslot-large .invslot-item").append("<div style=\"font-size: 12px;\">0</div>");
+        }
+        let invslot, item, amount
+        for (i = 0; i < $("#Editor .page:not(:hidden) tr").length - 1; i++) {
+            item = $("#Editor .page:not(:hidden) tr:eq(" + (i + 1) + ") td:eq(2) input").val().trim();
+            amount = $("#Editor .page:not(:hidden) tr:eq(" + (i + 1) + ") td:eq(3) input").val().trim();
+            $("#Editor .page:not(:hidden) tr:eq(" + (i + 1) + ") td:eq(1) input").val().split("").forEach(slot => {
+                if (item !== "") {
+                    if (slot === "0") {
+                        invslot = $("#Editor .page:not(:hidden) .invslot-large .invslot-item")
+                    } else {
+                        invslot = $("#Editor .page:not(:hidden) .ui-row:eq(" + ~~((+slot - 1)/3) +") .invslot:eq(" + ((+slot - 1) % 3) + ") .invslot-item")
+                    }
+                    invslot.children("*").remove()
+                    invslot.append("<a href=\"https://hypixel-skyblock.fandom.com/wiki/" + item + "\" target=\"_blank\"><div title=\"" + item + "\" style=\"font-size: 32px; text-align: center;\">" + item[0] + "</div></a>")
+                    if (amount !== "1") {
+                        invslot.append("<span class=\"invslot-stacksize\">" + amount + "</span>")
+                    }
+                }
+            });
+        }
+    }
+}
